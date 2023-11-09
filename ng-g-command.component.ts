798b4c2aba1465122875c8536f309516941054b1
ankita-4949 <139158023+ankita-4949@users.noreply.github.com>
2023-11-08 23:16:17 -0800

    Add files via upload

---
+++ ng-g-command.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-g-command',
+  templateUrl: './ng-g-command.component.html',
+  styleUrls: ['./ng-g-command.component.css']
+})
+export class NgGCommandComponent {
+
+}
